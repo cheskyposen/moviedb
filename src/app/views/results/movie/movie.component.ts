968a6966b70aa43f8137da344ee9b1b6f06fa446
cheskyposen <shragaposen@gmail.com>
2019-06-02 17:35:34 -0400

    styled and fixed routerlink

--- src/app/views/results/movie/movie.component.ts
+++ src/app/views/results/movie/movie.component.ts
@@ -12,6 +12,7 @@ export class MovieComponent {
   id: string;
   movie: Movie;
   constructor(private moviedb: MoviedbService, private route: ActivatedRoute) {
+    // subscribes to url params and searches movie by that id
     this.route.paramMap.subscribe((params) => {
       this.id = params.get('mid');
       this.moviedb.fetchMovieById(this.id).subscribe((res) => this.movie = new Movie(res));
--- src/app/views/results/movies/movies.component.ts
+++ src/app/views/results/movies/movies.component.ts
@@ -16,17 +16,18 @@ export class MoviesComponent implements OnInit {
     public moviedb: MoviedbService
   ) {}
   ngOnInit() {
+    // when going back it brings back the previous search
     if (this.moviedb.query) { this.movieSearch(); }
   }
-
+  // makes a http call requesting the movie in query
   movieSearch() {
     this.moviedb.movieSearch(this.moviedb.query).subscribe( results => {
-      console.log(results);
+      // console.log(results);
       this.movies = results;
       this.collectionSize = this.movies.length;
     });
   }
-
+  // gives the movies according to pagination options and position
   get moviesRes(): Movie[] {
     return this.movies
       .map((movie, i) => ({id: i + 1, ...movie}))
